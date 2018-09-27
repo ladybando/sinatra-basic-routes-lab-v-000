95d6f8b4de4908a066ade8067347b8f77ec54957
Ayana Bando <ayanaflax@yahoo.com>
2018-09-27 02:54:14 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -4,17 +4,14 @@ class App < Sinatra::Base
 
   get '/name' do
     "My name is #{name}"
-    Thin::HTTP_STATUS_CODES[200]
   end
 
   get '/hometown' do
     "My hometown is #{hometown}"
-    redirect 200
   end
   
     get '/favorite-song' do
       "My favorite song is #{favorite-song}"
-      redirect 200
-    end
+  end
 
 end
